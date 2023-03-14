df543320bc69508718446e4694dbfa38161ee19c
kuruvanaresh1 <99115993+kuruvanaresh1@users.noreply.github.com>
2023-03-14 23:11:58 +0530

    interestcalculation.java

---
+++ interestcalculation.java
@@ -0,0 +1,16 @@
+import java.util.*;  
+class interestcalculation   
+{  
+public static void main(String[] args)  
+{  
+Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+System.out.print("Enter your amount- ");  
+int p= sc.nextInt();  
+System.out.print("Enter your rate- ");  
+int r= sc.nextInt();  
+System.out.print("Enter your timeduration- ");  
+int t= sc.nextInt();  
+int d=(p * r * t)/100;  
+System.out.println("Total= " +d);  
+}  
+}  
